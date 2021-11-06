79f8dc42c6ac00d269e6d5f9482a5ceab25237cc
buglot <65818720+buglot@users.noreply.github.com>
2021-11-07 03:01:47 +0700

    commit

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ if Me.read_Token(Sheet_name='Token',Headcolumn_Name='Token'):
     print(Me.consored_Token(CountNumber_Show=20))
     bot=commands.Bot(command_prefix='-')
 
-
     @bot.event
     async def on_ready():
         print('Running.......')
